f75d4ab064eebce9df7c9c2d6ae84d094ba696d4
iMD14 <137671905+iMD14@users.noreply.github.com>
2023-07-10 07:15:52 +0300

    Add files via upload

---
+++ src/CoinsAPI.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace iMD14\CoinsAPI;
+
+use iMD14\CoinsAPI\event\EventListener;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class CoinsAPI extends PluginBase
+{
+
+    public static $instance;
+
+  public function onEnable(): void
+  {
+    @mkdir($this->getDataFolder());
+    $this->saveResource("players.yml");
+    $this->saveResource("config.yml");
+    self::$instance = $this;
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+  }
+
+    /**
+     * @param Player $player
+     * @return bool|mixed
+     */
+    public function getCoins(Player $player)
+    {
+      $config = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+      return $config->get($player->getName());
+    }
+
+    /**
+     * @param Player $player
+     * @param int $amount
+     */
+    public function addCoins(Player $player, int $amount)
+    {
+        $config = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+
+        $config->set(strtolower($player->getName()), $this->getCoins($player) + $amount);
+        $config->save();
+    }
+
+    /**
+     * @param Player $player
+     * @param int $amount
+     */
+    public function removeCoins(Player $player, int $amount)
+    {
+        $config = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+
+        $config->set(strtolower($player->getName()), $this->getCoins($player) + $amount);
+        $config->save();
+    }
+
+    /**
+     * @param Player $player
+     * @param int $amount
+     */
+    public function setCoins(Player $player, int $amount)
+    {
+        $config = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+
+        $config->set(strtolower($player->getName()), $amount);
+        $config->save();
+    }
+
+    public static function getInstance(): self
+    {
+        return self::$instance;
+    }
+
+}
---
+++ src/event/EventListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace iMD14\CoinsAPI\event;
+
+use iMD14\CoinsAPI\CoinsAPI;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\utils\Config;
+
+class EventListener implements Listener
+{
+
+  public function __construct(CoinsAPI $plugin)
+  {
+    $this->plugin = $plugin;
+  }
+
+  public function onLogin(PlayerLoginEvent $event)
+  {
+    $player = $event->getPlayer();
+    $config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML);
+    $config1 = new Config($this->plugin->getDataFolder() . "players.yml");
+    if(!$config->exists($player->getName())){
+      $config1->set($player->getName(), $config->get("StartingAmount"));
+      $config1->save();
+    }
+  }
+}
